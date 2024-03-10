521a0312055943be05b1b02f2e145f5dd75b6ab7
priyadarshiNishant <priyadarshinishant063@gmail.com>
2024-03-11 00:44:59 +0530

    Task5_ejs

---
+++ Task5_ejs/routes/shop.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/', productsController.getProducts);
+
+module.exports = router;
+
